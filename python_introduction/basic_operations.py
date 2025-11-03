dab9cb7e12d3e03d9b5bd9922cd645577ad66003
cyrilochako647-spec <cyrilochako647@gmail.com>
2025-11-03 20:30:54 +0300

    commit 1

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+
+number1 = 50
+number2 = 46
+
+
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+
+print(f"Addition of {number1} and {number2} is {addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,10 @@
+
+length = 27
+width = 16
+
+
+
+area = length * width
+
+
+print(f"The area of the rectangle is: {area}")
